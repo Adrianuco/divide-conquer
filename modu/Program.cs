a3c46f84e2fd008de3bd7535ecf8fc29cc7ad961
DereckSLV <159095741+DereckSLV@users.noreply.github.com>
2024-10-11 08:37:05 -0700

    Add files via upload

---
+++ modu/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modu
+{
+    public class Estudiante
+    {
+        public int id { get; set; }
+        public string nombre { get; set; }
+
+        // Constructor
+        public Estudiante(int id, string nombre)
+        {
+            this.id = id;
+            this.nombre = nombre;
+        }
+
+        // Método para mostrar detalles
+        public void MostrarDetalles()
+        {
+            Console.WriteLine($"ID: {id}, Nombre: {nombre}");
+        }
+    }
+
+    public class Asignatura
+    {
+        public int codigo { get; set; }
+        public string nombre { get; set; }
+
+        // Constructor
+        public Asignatura(int codigo, string nombre)
+        {
+            this.codigo = codigo;
+            this.nombre = nombre;
+        }
+
+        // Método para mostrar detalles
+        public void MostrarAsignatura()
+        {
+            Console.WriteLine($"Código: {codigo}, Nombre: {nombre}");
+        }
+    }
+}
